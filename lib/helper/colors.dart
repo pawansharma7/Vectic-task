6212e0507c702748ca66da28970b4f686699b92c
pawanbhardwaj <pawanbhardwaj756@gmail.com>
2022-08-24 10:08:03 +0530

    added final keyword

--- lib/helper/colors.dart
+++ lib/helper/colors.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/material.dart';
 
-Color primaryColor = Colors.pink;
-Color greyColor = Colors.grey[300]!;
+const Color primaryColor = Colors.pink;
+final Color greyColor = Colors.grey[300]!;
 
-Color darkGreyColor = Colors.grey[800]!;
+final Color darkGreyColor = Colors.grey[800]!;
 
-Color lightPink = Colors.pink.withOpacity(0.1);
+final Color lightPink = Colors.pink.withOpacity(0.1);
--- lib/ui/cart_screen.dart
+++ lib/ui/cart_screen.dart
@@ -39,84 +39,82 @@ class CartScreen extends StatelessWidget {
               ))
         ],
       ),
-      body: SingleChildScrollView(
-        child: Stack(
-          children: [
-            SizedBox(
-              height: size.height - (AppBar().preferredSize.height * 2),
-              child: Column(
-                children: [
-                  SelectedItemsCount(
-                      selected: response.selectedItems.length,
-                      totalPrice: response.totalPrice),
-                  Expanded(
-                    child: ListView.builder(
-                      shrinkWrap: true,
-                      physics: const ClampingScrollPhysics(),
-                      itemCount: 3,
-                      itemBuilder: (BuildContext context, int index) {
-                        return ProductWidget(
-                          productImage: productImages[index],
-                          productDescription: productDesc[index],
-                          productName: productName[index],
-                          productSoldBy: productSoldBy[index],
-                          productSize: productSize[index],
-                          productQty: productQty[index],
-                          productLeft: productLeft[index],
-                          productPrice: productPrice[index],
-                          productOGPrice: productOgPrice[index],
-                        );
-                      },
-                    ),
+      body: Stack(
+        children: [
+          SizedBox(
+            height: size.height - (AppBar().preferredSize.height * 2),
+            child: Column(
+              children: [
+                SelectedItemsCount(
+                    selected: response.selectedItems.length,
+                    totalPrice: response.totalPrice),
+                Expanded(
+                  child: ListView.builder(
+                    shrinkWrap: true,
+                    physics: const ClampingScrollPhysics(),
+                    itemCount: 3,
+                    itemBuilder: (BuildContext context, int index) {
+                      return ProductWidget(
+                        productImage: productImages[index],
+                        productDescription: productDesc[index],
+                        productName: productName[index],
+                        productSoldBy: productSoldBy[index],
+                        productSize: productSize[index],
+                        productQty: productQty[index],
+                        productLeft: productLeft[index],
+                        productPrice: productPrice[index],
+                        productOGPrice: productOgPrice[index],
+                      );
+                    },
                   ),
-                  response.selectedItems.isNotEmpty ?  SizedBox(
-                      height: getProportionateScreenHeight(93)) : const SizedBox()
-                ],
-              ),
+                ),
+                response.selectedItems.isNotEmpty ?  SizedBox(
+                    height: getProportionateScreenHeight(93)) : const SizedBox()
+              ],
             ),
-            response.selectedItems.isNotEmpty
-                ? Positioned(
-                    bottom: 0,
-                    child: Column(
-                      mainAxisSize: MainAxisSize.min,
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        Container(
-                          padding: const EdgeInsets.symmetric(vertical: 5),
-                          decoration: BoxDecoration(color: lightPink),
-                          width: size.width,
-                          alignment: Alignment.center,
-                          child: Text(
-                            response.selectedItems.length.toString() +
-                                " Items selected for this order",
-                            style: const TextStyle(fontWeight: FontWeight.w600),
-                          ),
+          ),
+          response.selectedItems.isNotEmpty
+              ? Positioned(
+                  bottom: 0,
+                  child: Column(
+                    mainAxisSize: MainAxisSize.min,
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Container(
+                        padding: const EdgeInsets.symmetric(vertical: 5),
+                        decoration: BoxDecoration(color: lightPink),
+                        width: size.width,
+                        alignment: Alignment.center,
+                        child: Text(
+                          response.selectedItems.length.toString() +
+                              " Items selected for this order",
+                          style: const TextStyle(fontWeight: FontWeight.w600),
                         ),
-                        Container(
-                          alignment: Alignment.bottomCenter,
-                          padding: const EdgeInsets.only(
-                              top: 11,right: 15, left: 15),
-                          child: MaterialButton(
-                            shape: RoundedRectangleBorder(
-                                borderRadius: BorderRadius.circular(5)),
-                            height: getProportionateScreenHeight(55),
-                            minWidth: size.width - 30,
-                            onPressed: () {},
-                            child: Text(
-                              "Place Order".toUpperCase(),
-                              style: Styles().materialButton.copyWith(
-                                  fontWeight: FontWeight.w500,
-                                  letterSpacing: 1.5),
-                            ),
-                            color: primaryColor,
+                      ),
+                      Container(
+                        alignment: Alignment.bottomCenter,
+                        padding: const EdgeInsets.only(
+                            top: 11,right: 15, left: 15),
+                        child: MaterialButton(
+                          shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(5)),
+                          height: getProportionateScreenHeight(55),
+                          minWidth: size.width - 30,
+                          onPressed: () {},
+                          child: Text(
+                            "Place Order".toUpperCase(),
+                            style: Styles().materialButton.copyWith(
+                                fontWeight: FontWeight.w500,
+                                letterSpacing: 1.5),
                           ),
-                        )
-                      ],
-                    ),
-                  )
-                : const SizedBox()
-          ],
-        ),
+                          color: primaryColor,
+                        ),
+                      )
+                    ],
+                  ),
+                )
+              : const SizedBox()
+        ],
       ),
     );
   }
